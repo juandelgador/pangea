61732b8ea11e0219bc738c3bc242006f7c1459ac
luis eduardo conejeros montecino <sandokan42@hotmail.com>
2016-04-26 15:24:35 +0200

    se agrego rol controlador y vista

---
+++ Pangea/Controllers/RolController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Pangea.Models;
+
+namespace Pangea.Controllers
+{
+    public class RolController : Controller
+    {
+        private PangeaEntities db = new PangeaEntities();
+
+        // GET: Rol
+        public ActionResult Index()
+        {
+            return View(db.Rol.ToList());
+        }
+
+        // GET: Rol/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Rol rol = db.Rol.Find(id);
+            if (rol == null)
+            {
+                return HttpNotFound();
+            }
+            return View(rol);
+        }
+
+        // GET: Rol/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Rol/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,nombre,habilitado,id_trabajador_crea,fecha_crea,id_trabajador_modif,fecha_modif")] Rol rol)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Rol.Add(rol);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(rol);
+        }
+
+        // GET: Rol/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Rol rol = db.Rol.Find(id);
+            if (rol == null)
+            {
+                return HttpNotFound();
+            }
+            return View(rol);
+        }
+
+        // POST: Rol/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,nombre,habilitado,id_trabajador_crea,fecha_crea,id_trabajador_modif,fecha_modif")] Rol rol)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(rol).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(rol);
+        }
+
+        // GET: Rol/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Rol rol = db.Rol.Find(id);
+            if (rol == null)
+            {
+                return HttpNotFound();
+            }
+            return View(rol);
+        }
+
+        // POST: Rol/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Rol rol = db.Rol.Find(id);
+            db.Rol.Remove(rol);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
